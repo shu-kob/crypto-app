b99cd53d131d1db2359b89f07362c2d3a530febb
shu.kobuchi <shu.kobuchi@bitcoinbank.co.jp>
2021-08-05 23:11:50 +0900

    display private key

--- getRandomAddress.ts
+++ getRandomAddress.ts
@@ -2,6 +2,8 @@ let bitcoin = require('bitcoinjs-lib');
 
 const keyPair = bitcoin.ECPair.makeRandom();
 
+console.log("private key WIF:\n" + keyPair.toWIF());
+
 const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
 
-console.log(address);
+console.log("random address:\n" + address);
