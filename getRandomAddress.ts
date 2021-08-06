86346466b4c0352ca9bed94e7e256d0974e4247f
shu.kobuchi <shu.kobuchi@bitcoinbank.co.jp>
2021-08-07 00:18:27 +0900

    Add getting testnet address

--- getRandomAddress.ts
+++ getRandomAddress.ts
@@ -1,9 +1,20 @@
 let bitcoin = require('bitcoinjs-lib');
+const TESTNET = bitcoin.networks.testnet;
 
 const keyPair = bitcoin.ECPair.makeRandom();
 
 console.log("private key WIF:\n" + keyPair.toWIF());
 
-const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+const address = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
 
-console.log("random address:\n" + address);
+console.log("random address:\n" + address.address);
+
+const testnetKeyPair = bitcoin.ECPair.makeRandom({ network: TESTNET });
+
+console.log("testnet private key WIF:\n" + testnetKeyPair.toWIF());
+
+const testnetAddress = bitcoin.payments.p2pkh({
+      pubkey: testnetKeyPair.publicKey,
+      network: TESTNET,
+    });
+console.log("testnet random address:\n" + testnetAddress.address);
