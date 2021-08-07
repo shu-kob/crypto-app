266669ee9edeb9db52d40f60774765f1ce545a24
shu-kob <shukobuchi@gmail.com>
2021-08-07 17:17:57 +0900

    refactor const

--- getAddress.ts
+++ getAddress.ts
@@ -3,18 +3,20 @@ const TESTNET = bitcoin.networks.testnet;
 
 function getRandomPrivKey(){
     const keyPair = bitcoin.ECPair.makeRandom();
-    console.log("private key WIF:\n" + keyPair.toWIF());
+    const privKey = keyPair.toWIF()
+    console.log("private key WIF:\n" + privKey);
     const address = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
     console.log("random address:\n" + address.address);
-    return keyPair.toWIF();
+    return privKey;
 }
 
 function getTestnetRandomPrivKey(){
     const testnetKeyPair = bitcoin.ECPair.makeRandom({ network: TESTNET });
-    console.log("testnet private key WIF:\n" + testnetKeyPair.toWIF());
+    const testnetPrivKey = testnetKeyPair.toWIF();
+    console.log("testnet private key WIF:\n" + testnetPrivKey);
     const testnetAddress = bitcoin.payments.p2pkh({ pubkey: testnetKeyPair.publicKey, network: TESTNET, });
     console.log("testnet random address:\n" + testnetAddress.address);
-    return testnetKeyPair.toWIF();
+    return testnetPrivKey;
 }
 
 const RandomPrivKey = getRandomPrivKey();
