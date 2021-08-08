f62314e9572d284898a5cc5388515fb107340deb
shu-kob <shukobuchi@gmail.com>
2021-08-08 13:42:10 +0900

    Add getP2shAddress function

--- getAddress.ts
+++ getAddress.ts
@@ -19,10 +19,32 @@ function getTestnetRandomPrivKey(){
     return testnetPrivKey;
 }
 
-const RandomPrivKey = getRandomPrivKey();
+function getP2shAddress(){
+    const pubkeys = [
+        '026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01',
+        '02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9',
+        '03c6103b3b83e4a24a0e33a4df246ef11772f9992663db0c35759a5e2ebf68d8e9',
+    ].map(hex => Buffer.from(hex, 'hex'));
+    const { address } = bitcoin.payments.p2sh({
+        redeem: bitcoin.payments.p2ms({ m: 2, pubkeys }),
+    });
+    return address;
+}
 
-console.log(RandomPrivKey);
+function getP2shTestnetAddress(){
+    const pubkeys = [
+        '026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01',
+        '02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9',
+        '03c6103b3b83e4a24a0e33a4df246ef11772f9992663db0c35759a5e2ebf68d8e9',
+    ].map(hex => Buffer.from(hex, 'hex'));
+    const { address } = bitcoin.payments.p2sh({
+        redeem: bitcoin.payments.p2ms({ m: 2, pubkeys, network: TESTNET, }),
+    });
+    return address;
+}
 
-const RandomTestnetPrivKey = getTestnetRandomPrivKey();
+const p2phAddress = getP2shAddress();
+console.log(p2phAddress);
 
-console.log(RandomTestnetPrivKey);
+const p2phTestnetAddress = getP2shTestnetAddress();
+console.log(p2phTestnetAddress);
