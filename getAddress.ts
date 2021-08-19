cf1a0487e9a0a69007283c89ca669634d4765ada
shu-kob <shukobuchi@gmail.com>
2021-08-19 23:00:12 +0900

    Add P2SH-P2WSH Address

--- getAddress.ts
+++ getAddress.ts
@@ -189,3 +189,37 @@ function getSegwitMultisigTestnetAddress(){
 
 const segwitMultisigTestnetAddress = getSegwitMultisigTestnetAddress();
 console.log("p2wsh testnet: " + segwitMultisigTestnetAddress);
+
+function getP2SHP2WSHAddress(){
+    const pubkeys = [
+        makePubKey(1),
+        makePubKey(1),
+        makePubKey(1),
+    ].map(Buffer => Buffer);
+    const { address } = bitcoin.payments.p2sh({
+        redeem: bitcoin.payments.p2wsh({
+            redeem: bitcoin.payments.p2ms({ m: 2, pubkeys }),
+        }),
+    });
+    return address;
+}
+
+const P2SHP2WSHAddress = getP2SHP2WSHAddress();
+console.log("P2SHP2WSHAddress: " + P2SHP2WSHAddress);
+
+function getP2SHP2WSHTestnetAddress(){
+    const pubkeys = [
+        '03380eaa099fed61543d27cad48b4288c65a25cd3509ba0181c2f26d1750a6f8ec',
+        '02f228a300c0d3310deed09b16ead058f2afc519ab39a3906eaf04b1ad85c6c64f',
+        '02c5828ef798fe715c97ecb03564a5b11bb4bf7e408c6813aa51335f6e9e39b4de',
+    ].map(hex => Buffer.from(hex, 'hex'));
+    const { address } = bitcoin.payments.p2sh({
+        redeem: bitcoin.payments.p2wsh({
+            redeem: bitcoin.payments.p2ms({ m: 2, pubkeys, network: TESTNET, }),
+        }),
+    });
+    return address;
+}
+
+const P2SHP2WSHTestnetAddress = getP2SHP2WSHTestnetAddress();
+console.log("P2SHP2WSHTestnetAddress: " + P2SHP2WSHTestnetAddress);
