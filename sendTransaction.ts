2e687b5a895d21d0203eb6aca9c316c5c3415279
shu-kob <shukobuchi@gmail.com>
2021-08-23 19:19:13 +0900

    refactoring

--- sendTransaction.ts
+++ sendTransaction.ts
@@ -4,12 +4,13 @@ import { BIP32Interface } from 'bip32';
 import * as bip39 from 'bip39';
 import * as wif from 'wif';
 const TESTNET = bitcoin.networks.testnet;
+let bitcoinNetwork = TESTNET;
 
 function mnemonicToPubKey(addressIndex: number) {
     const mnemonic: any = process.env.MNEMONIC;
     console.log("mnemonic: " + mnemonic)
     const seed = bip39.mnemonicToSeedSync(mnemonic);
-    const node = bip32.fromSeed(seed);
+    const node = bip32.fromSeed(seed, bitcoinNetwork);
     const xpriv = node.toBase58();
     console.log("xpriv: " + xpriv);
     const xpub = node.neutered().toBase58();
@@ -20,21 +21,21 @@ function mnemonicToPubKey(addressIndex: number) {
     console.log("privKey2: " + privKey2);
     const pubkey = node.derivePath("m/44'/0/0/0/0").publicKey;
     console.log(pubkey);
-    let bip32Interface: BIP32Interface = bip32.fromBase58(xpub);
+    let bip32Interface: BIP32Interface = bip32.fromBase58(xpub, bitcoinNetwork);
     return bip32Interface.derive(addressIndex).publicKey;
 }
 
 function getp2wpkhAddress(addressIndex: number){
     const pubkey_buf = mnemonicToPubKey(addressIndex);
     console.log(pubkey_buf);
-    const address = bitcoin.payments.p2wpkh({ pubkey: pubkey_buf, network: TESTNET, }).address;
+    const address = bitcoin.payments.p2wpkh({ pubkey: pubkey_buf, network: bitcoinNetwork, }).address;
     return address;
 }
 
 function mnemonicToPrivKey(addressIndex: number) {
     const mnemonic: any = process.env.MNEMONIC;
     const seed = bip39.mnemonicToSeedSync(mnemonic);
-    const node = bip32.fromSeed(seed);
+    const node = bip32.fromSeed(seed, bitcoinNetwork);
     const xpriv = node.toBase58();
     console.log("xpriv: " + xpriv);
     const privKey = node.derive(addressIndex).toWIF();
@@ -45,7 +46,7 @@ function mnemonicToPrivKey(addressIndex: number) {
 const address = getp2wpkhAddress(0);
 console.log(address);
 
-const psbt = new bitcoin.Psbt({ network: TESTNET });
+const psbt = new bitcoin.Psbt({ network: bitcoinNetwork });
 
 psbt.addInput({
     hash: 'f0dcde34d982e476389986e0277f7e1858c2b55c16ddf59306f20c1a950f61a8',
@@ -68,9 +69,9 @@ psbt.addOutput({
 const privateKey_wif = mnemonicToPrivKey(0);
 
 const keyPair = bitcoin.ECPair.fromWIF(
-    privateKey_wif,
+    privateKey_wif, bitcoinNetwork
   );
-  const address_from_wif = bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey, network: TESTNET });
+  const address_from_wif = bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey, network: bitcoinNetwork });
 
 console.log("address_from_wif: " + address_from_wif.address);
 
