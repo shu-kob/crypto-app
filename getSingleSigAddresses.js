670db2047885fbb75d42935a4dcddef2301ec29b
shu-kob <shukobuchi@gmail.com>
2021-08-31 22:49:12 +0900

    refactor getSingleSigAddresses.js

--- getSingleSigAddresses.js
+++ getSingleSigAddresses.js
@@ -7,8 +7,13 @@ const TESTNET = bitcoin.networks.testnet;
 // let bitcoinNetwork = MAINNET;
 let bitcoinNetwork = TESTNET;
 
-const pubkeyNode = bitcoin.bip32.fromBase58(xpub, bitcoinNetwork);
-const pubkey = pubkeyNode.derive(0).derive(0).publicKey;
+function getPublicKey(xpub, addressIndex){
+    const pubkeyNode = bitcoin.bip32.fromBase58(xpub, bitcoinNetwork);
+    const pubkey = pubkeyNode.derive(0).derive(addressIndex).publicKey;
+    return pubkey
+}
+
+const pubkey = getPublicKey(xpub, 0);
 
 function getP2pkhAddress(){
     const address = bitcoin.payments.p2pkh({ pubkey: pubkey, network: bitcoinNetwork, }).address;
@@ -27,14 +32,14 @@ function getP2wpkhAddress(){
     return address;
 }
 
-const p2pkhAddress = getP2pkhAddress();
+const p2pkhAddress = getP2pkhAddress(0);
 console.log("P2PKH:");
 console.log(p2pkhAddress);
 
-const p2shP2wpkhAddress = getP2shP2wpkhAddress();
+const p2shP2wpkhAddress = getP2shP2wpkhAddress(0);
 console.log("P2SH-P2WPKH:");
 console.log(p2shP2wpkhAddress);
 
-const p2wpkhAddress = getP2wpkhAddress();
+const p2wpkhAddress = getP2wpkhAddress(0);
 console.log("P2WPKH:");
 console.log(p2wpkhAddress);
