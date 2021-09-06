52aa5e38ba082f01df27ec2aa621ab4210eb098c
shu-kob <shukobuchi@gmail.com>
2021-09-06 23:59:10 +0900

    fix derivePath and refactor at p2shP2wpkhTx.js

--- p2shP2wpkhTx.js
+++ p2shP2wpkhTx.js
@@ -10,14 +10,19 @@ let bitcoinNetwork = TESTNET;
 
 const { xpriv } = require('./xpriv.json');
 
-const privkeyNode = bitcoin.bip32.fromBase58(xpriv, bitcoinNetwork);
-const privateKey_wif = privkeyNode.derive(1).derive(0).derive(0).derive(0).toWIF();
-console.log("privateKey_wif:");
-console.log(privateKey_wif);
+function getPrivKey(xpriv, addressIndex){
+    const privkeyNode = bitcoin.bip32.fromBase58(xpriv, bitcoinNetwork);
+    const privateKey_wif = privkeyNode.derive(0).derive(addressIndex).toWIF();
+    const obj = wif.decode(privateKey_wif);
+    const privKey = bitcoin.ECPair.fromPrivateKey(obj.privateKey);
+    return privKey;
+}
+
+const privkey = getPrivKey(xpriv, 0);
 
 function getPubkeyFromXpub(xpub) {
     const pubkeyNode = bitcoin.bip32.fromBase58(xpub, bitcoinNetwork);
-    const pubkey = pubkeyNode.derive(1).derive(0).derive(0).derive(0).publicKey;
+    const pubkey = pubkeyNode.derive(0).derive(0).publicKey;
     return pubkey;
 }
 
@@ -34,29 +39,24 @@ console.log(p2sh.address)
 
 const psbt = new bitcoin.Psbt({ network: bitcoinNetwork });
 
-const previousRawTx = '02000000000101387eee5dd96c6349fb16ceef466c0b9265e029cd71abe47d7ee0a08621424c360100000000feffffff02b379920000000000160014a13008ccc2f7b83463984b5b4afa65fbbdf87851a08601000000000017a9149c966478e58697e07ba4aec10fa6f3508f4325fa870247304402205dd9a3c774fc720dd7855c79baa19a28b025ff9596959b05dd2767fa22bbe24602201969d84c378222af595bf2addeb929a2dd74e906dc806b2418e47622b18ca8e0012102a37d8b815cea0042b01ce49c9f83ab942c8e4b8a85b3c7aca5b641120d654ea179ce0000';
+const previousRawTx = '0200000000010171ddd098f38f5d505907fc981c85027d2935cb66a558bfdd010159ff1b89d8e20000000000feffffff0256caa8ab000000001600144dcc600a84cbf35fa6b44ad2ec5ba3dc6cd55034809698000000000017a914d2a8c75cc68bf3e5be639a9ed433631585538a5787024730440220169792f348182b450cf75e011c88586bdf0bcb4d0093c6d9b059b846a4c3422602207762963678c9836df3729f1a7adee12cadc34b7389dd659c170d0ec7813e61cc012102099874d0b4dbfcba94fe4f84f6def3d4bb0cd56b76ca9cba1476b95df46f9530e6d30000';
 
 psbt.addInput({
-    hash: 'bb52d8788b686b52c31b1c9998176207a295c513b3254b8e793eb015c22f2ade',
+    hash: 'b95edf41c5326c6e5836db49242a949be1161bbce327c2599cef89d5773fd1fa',
     index: 1,
     redeemScript: p2wpkh.output,
     nonWitnessUtxo: Buffer.from(previousRawTx, 'hex'),
 });
 psbt.addOutput({
-    address: "tb1q2adzgyzuy3msvjjfn3pkghf5fnwtwmxcglw3cuk5gvvv3ahmt63qqqa3n6",
-    value: 30000,
+    address: "tb1qh9em5av6lqzhkf3jjpv447r4zqfxp36fg79c9y",
+    value: 5000000,
 });
 psbt.addOutput({
-    address: "tb1qzwwvqxr2m8l0kt8trnnhaa48awtq45zauflz5y",
-    value: 69824,
+    address: "tb1qs0yawew4wp90rxtspwzugen077h5anh5xf5al9",
+    value: 4999836,
 });
 
-const obj = wif.decode(privateKey_wif);
-
-const privKey = bitcoin.ECPair.fromPrivateKey(obj.privateKey);
-
-psbt.signInput(0, privkey1)
-psbt.signInput(0, privkey2);
+psbt.signInput(0, privkey);
 
 psbt.validateSignaturesOfInput(0);
 psbt.finalizeAllInputs();
