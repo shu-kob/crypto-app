95c4ebf3b3b5177f762f3bf40c849d816cd8292e
小渕周 <shukobuchi@gmail.com>
2022-04-15 23:40:24 +0900

    support qtum addresses

--- getSingleSigAddresses.js
+++ getSingleSigAddresses.js
@@ -4,7 +4,43 @@ const bip39 = require('bip39');
 const { xpub } = require('./xpub.json');
 const MAINNET = bitcoin.networks.bitcoin;
 const TESTNET = bitcoin.networks.testnet;
-let bitcoinNetwork = TESTNET; // MAINNET or TESTNET
+
+const LITECOIN = {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bech32: 'ltc',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe,
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x32,
+    wif: 0xb0,
+};
+
+const DOGECOIN = {
+    messagePrefix: '\x19Dogecoin Signed Message:\n',
+    bip32: {
+      public: 0x02facafd,
+      private: 0x02fac398
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x16,
+    wif: 0x9e
+};
+
+const QTUM = {
+    messagePrefix: '\x19Qtum Signed Message:\n',
+    bech32: 'qc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4,
+    },
+    pubKeyHash: 0x3A,
+    scriptHash: 0x32,
+    wif: 0x80
+};
+
+let bitcoinNetwork = TESTNET; // MAINNET, TESTNET, LITECOIN, DOGECOIN or QTUM
 
 let purpose = "44"
 
@@ -16,10 +52,19 @@ if (bitcoinNetwork == MAINNET) {
 else if (bitcoinNetwork == TESTNET) {
     coinType = "1";
 }
+else if (bitcoinNetwork == LITECOIN) {
+    coinType = "2";
+}
+else if (bitcoinNetwork == DOGECOIN) {
+    coinType = "3";
+}
+else if (bitcoinNetwork == QTUM) {
+    coinType = "2301";
+}
 
 let account = "0"
 
-let nonChangeAddress = 0;
+let externalAddress = 0;
 let changeAddress = 1;
 
 function getPublicKey(xpub, isChange, addressIndex){
@@ -48,9 +93,9 @@ function getP2wpkhAddress(xpub, isChange, addressIndex){
     return address;
 }
 
-let isChange = nonChangeAddress;
+let isChange = externalAddress;
 
-console.log("Non Change Addresses");
+console.log("External Addresses");
 
 for (let addressIndex = 0; addressIndex < 5; addressIndex++){
 
